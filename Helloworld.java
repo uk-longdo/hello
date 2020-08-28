bb7e96dcd8ec314c37507dbd27f442357c406b12
uk-longdo <70267144+uk-longdo@users.noreply.github.com>
2020-08-28 22:15:41 +0900

    add for
    9에서 멈추기!

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,10 @@
 class Helloworld {
     public static void main(String[] args){
         System.out.println("world hello");
+        
+        for(int a = 1; a < 10; a++) {
+            System.out.println("1씩추가되서 9에멈춰줘" +a);
+        }
+            
     }
 }
