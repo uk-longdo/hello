41381ec38e892ed73ac6b0a04007bf915ce00bab
uk-longdo <70267144+uk-longdo@users.noreply.github.com>
2020-09-03 18:37:52 +0900

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -5,6 +5,12 @@ class Helloworld {
         for(int a = 1; a < 10; a++) {
             System.out.println("1씩추가되서 9에멈춰줘" +a);
         }
+        String[] 학생 = { "민지", "민욱", "사람"};
+		for (int i= 0; i < 학생.length; i++) {
+			String 학생은 = 학생[i];
+			System.out.println(학생은 + "은(는) 상담을 받았습니다.");
+		}
+		
             
     }
 }
